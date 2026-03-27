5a1e7c69b2d6c59013a6649a5d94c0240b36df6d
jayesh6060 <jayeshsuthar0621@gmail.com>
2026-03-27 15:25:07 +0530

    Configure GitHub Pages deployment via Actions

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  basePath: '/Web-Harmonium',
+  assetPrefix: '/Web-Harmonium/',
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
